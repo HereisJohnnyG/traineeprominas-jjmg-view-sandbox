ac1cf38d0fb027d3d33ce3189e460ea993ae5853
Jefferson <je_jmgbrasil@hotmail.com>
2019-06-19 17:55:22 -0300

    user details

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from 'src/model/user';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-
-  constructor() { }
+  user: User = { id: null, name: '', lastName: '', profile: '' };
+  isLoadingResults = true;
+  
+  constructor(private router: Router, private route: ActivatedRoute, private api: UserService) {}
 
   ngOnInit() {
+    this.getUser(this.user.snapshot.params['id']);
   }
-
+  
+  getUser(id) {
+    this.api.getUser(id)
+      .subscribe(data => {
+        this.user = data;
+        console.log(this.user);
+        this.isLoadingResults = false;
+      });
+  }
+  deleteProduto(id) {
+    this.isLoadingResults = true;
+    this.api.deleteProduto(id)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/produtos']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
